f08bddb30ae828a9243e62edeb0d862eb5a5f993
Zoya <ezoya@yahoo.com>
2023-07-28 14:07:37 +0100

    Initial Commit

---
+++ To-do list/To_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_listApp.swift
+//  To-do list
+//
+//  Created by scholar on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct To_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
